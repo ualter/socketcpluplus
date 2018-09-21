2273a342bcfadd2eae4e8c5a194bcbe204677743
Ualter Azambuja <ualter.junior@gmail.com>
2018-09-21 22:07:16 +0200

    .

--- SocketApplication/MultipleSocketApplication.cpp
+++ SocketApplication/MultipleSocketApplication.cpp
@@ -1,71 +1,29 @@
 #include "pch.h"
-#include <cpprest/http_client.h>
-#include <cpprest/filestream.h>
-//#include <cpprest/http_listener.h>              // HTTP server
-//#include <cpprest/json.h>                       // JSON library
-//#include <cpprest/uri.h>                        // URI library
-//#include <cpprest/ws_client.h>                  // WebSocket client
-//#include <cpprest/containerstream.h>            // Async streams backed by STL containers
-//#include <cpprest/interopstream.h>              // Bridges for integrating Async streams with STL and WinRT streams
-//#include <cpprest/rawptrstream.h>               // Async streams backed by raw pointer to memory
-//#include <cpprest/producerconsumerstream.h>     // Async streams for producer consumer scenarios
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+#include <cpprest/ws_client.h>
 
-using namespace utility;                    // Common utilities like string conversions
-using namespace web;                        // Common features like URIs.
-using namespace web::http;                  // Common HTTP functionality
-using namespace web::http::client;          // HTTP client features
-using namespace concurrency::streams;       // Asynchronous streams
+using namespace std;
+using namespace web;
+using namespace web::websockets::client;
 
-// NOT USED
-//using namespace web::http::experimental::listener;          // HTTP server
-//using namespace web::experimental::web_sockets::client;     // WebSockets client
-//using namespace web::json;                                  // JSON library
-
-
-// test with: ncat localhost 8888
 int main(int argc, char *argv[])
 {
-	auto fileStream = std::make_shared<ostream>();
+	websocket_client client;
 
-	// Open stream to output file.
-	pplx::task<void> requestTask = fstream::open_ostream(U("results.html")).then([=](ostream outFile)
-	{
-		*fileStream = outFile;
+	uri link = uri(L"ws://localhost:8080/websocket/chat/Iron");
+	client.connect(link).wait();
 
-		// Create http_client to send the request.
-		http_client client(U("http://www.bing.com/"));
+	websocket_outgoing_message out_msg;
+	out_msg.set_utf8_message("test");
+	client.send(out_msg).wait();
 
-		// Build request URI and start the request.
-		uri_builder builder(U("/search"));
-		builder.append_query(U("q"), U("cpprestsdk github"));
-		return client.request(methods::GET, builder.to_string());
-	})
+	client.receive().then([](websocket_incoming_message in_msg) {
+		return in_msg.extract_string();
+	}).then([](string body) {
+		cout << body << endl; // test
+	}).wait();
 
-	// Handle response headers arriving.
-	.then([=](http_response response)
-	{
-		printf("Received response status code:%u\n", response.status_code());
-
-		// Write response body into the file.
-		return response.body().read_to_end(fileStream->streambuf());
-	})
-
-	// Close the file stream.
-	.then([=](size_t)
-	{
-		return fileStream->close();
-	});
-
-	// Wait for all the outstanding I/O to complete and handle any exceptions
-	try
-	{
-		requestTask.wait();
-	}
-	catch (const std::exception &e)
-	{
-		printf("Error exception:%s\n", e.what());
-	}
+	client.close().wait();
 
 	return 0;
 }
